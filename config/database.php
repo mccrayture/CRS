efbfef3d04a9decf8658ad58bac6a522906b67f2
mccrayture <shikarumccrayture@gmail.com>
2015-11-16 15:28:45 +0700

    :octocat: Added .gitattributes & .gitignore files

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'database');
+define('DB_NAME', 'db_service');
+define('DB_USER', 'sa');
+define('DB_PASS', '#scph#');
+
+define('DB_USER_TYPE', 'mysql');
+define('DB_USER_HOST', '192.168.2.250');
+define('DB_USER_NAME', 'db_skh');
+define('DB_USER_USER', 'sa');
+define('DB_USER_PASS', 'scph');
